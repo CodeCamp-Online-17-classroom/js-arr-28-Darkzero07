9ff0dc034f76b1827cb9004766b6a04b897882bd
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-19 07:51:28 +0700

    js lab 114

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// write code here
+const tasks = [
+  { id: 1, name: 'Fishing' },
+  { id: 2, name: 'Shopping' },
+  { id: 3, name: 'Swimming' }
+];
+
+
+function deleteTask(id) {
+    const index =  tasks.findIndex((index) => index.id === id);
+    if (index ) {
+        tasks.splice(index, 1)
+    }
+}
+
+deleteTask(2);
+console.log(tasks);
+// [
+//   { id: 1, name: 'Fishing' },
+//   { id: 3, name: 'Swimming' }
+// ]
